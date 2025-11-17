a8fe7772ecf22a3e59309fbe779536acd21b8668
jaimeverdugodev-code <jaimeverdugo.dev@gmail.com>
2025-11-17 19:14:18 +0100

    cambios en los INSERT

--- src/main/resources/db/migration/V2__seed.sql
+++ src/main/resources/db/migration/V2__seed.sql
@@ -1,8 +1,8 @@
 INSERT INTO tipos_camisa (id, nombre, descripcion) VALUES
-(1, 'Oxford',   'Algodón tejido tipo Oxford'),
+(1, 'Oxford', 'Algodón tejido tipo Oxford'),
 (2, 'Camiseta', 'Tejido jersey, cuello redondo'),
-(3, 'Lino',     'Ligera y transpirable para verano'),
-(4, 'Polo',     'Piqué con cuello y botones')
+(3, 'Lino', 'Ligera y transpirable para verano'),
+(4, 'Polo', 'Piqué con cuello y botones')
 ON CONFLICT (id) DO NOTHING;
 
 INSERT INTO camisas (nombre, talla, color, precio, imagen_url, lat, lng, tipo_id) VALUES
@@ -10,4 +10,5 @@ INSERT INTO camisas (nombre, talla, color, precio, imagen_url, lat, lng, tipo_id
 ('Camiseta Negra Básica', 'S', 'Negro', 12.99, NULL, NULL, NULL, 2),
 ('Lino Natural', 'M', 'Beige', 49.00, NULL, NULL, NULL, 3),
 ('Polo Verde', 'M', 'Verde', 24.90, NULL, NULL, NULL, 4)
-ON CONFLICT DO NOTHING;
+ON CONFLICT (nombre) DO NOTHING;
+
