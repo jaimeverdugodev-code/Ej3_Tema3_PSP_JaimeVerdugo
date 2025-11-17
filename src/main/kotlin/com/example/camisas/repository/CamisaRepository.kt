93eb6d7db7c1f172e5659d96bddb1b2caae921ed
jaimeverdugodev-code <jaimeverdugo.dev@gmail.com>
2025-11-17 17:10:48 +0100

    borrar cambios en CamisaService y CamisaRepository

--- src/main/kotlin/com/example/camisas/repository/CamisaRepository.kt
+++ src/main/kotlin/com/example/camisas/repository/CamisaRepository.kt
@@ -2,13 +2,5 @@ package com.example.camisas.repository
 
 import com.example.camisas.domain.Camisa
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 
-interface CamisaRepository : JpaRepository<Camisa, Long> {
-
-    @Query("SELECT c FROM Camisa c JOIN FETCH c.tipo")
-    fun findAllWithTipo(): List<Camisa>
-
-    @Query("SELECT c FROM Camisa c JOIN FETCH c.tipo WHERE c.id = :id")
-    fun findByIdWithTipo(id: Long): Camisa?
-}
+interface CamisaRepository : JpaRepository<Camisa, Long>
